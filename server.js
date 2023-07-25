fd03f3ccc1d495b4854c69a55f728a735e340f4c
CORP\mahomsi <marwa.homsi@capgemini.com>
2023-07-25 14:53:38 +0200

    build node project structure

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ dotEnv.config({path: './config/config.env'});
 
 const app = express();
 
+const routes = require('./routes/routes.js');
+app.use('/api/v1/bootcamps', routes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`server running on ${process.env.NODE_ENV} mode on Port ${process.env.PORT}`);
